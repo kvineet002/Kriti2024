11184453eb6b448fa2208960f01802cf7eed59f9
darkmatter <vikasanand.darkmatter@gmail.com>
2024-01-31 00:10:07 +0530

    Changes made

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,20 +17,20 @@ function Navbar() {
         <div className="text-sm lg:flex-grow">
           <form className="flex">
             <input type="text" placeholder="Search" className="px-4 py-2 rounded-lg border-transparent focus:border-gray-300 focus:outline-none focus:shadow-outline-blue bg-gray-100 bg-opacity-25 form-input w-full mr-2"/>
-            <button type="submit" className="flex items-center bg-gray-100 bg-opacity-25 hover:bg-gray-200 hover:bg-opacity-50 text-gray-600 font-semibold py-2 px-4 rounded-lg">
+            <button type="submit" className="flex items-center bg-gray-100 bg-opacity-25 hover:bg-gray-200 hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-lg">
               <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35-5.33a8 8 0 0 1 0 1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
               Search
             </button>
           </form>
         </div>
         <div>
-          <button className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:underline mr-4">
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-white hover:underline mr-4">
             Profile
           </button>
-          <button className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:underline mr-4">
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-white hover:underline mr-4">
             Projects
           </button>
-          <button className="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:underline">
+          <button className="block mt-4 lg:inline-block lg:mt-0 text-white hover:underline">
             Courses
           </button>
         </div>
