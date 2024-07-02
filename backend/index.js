75e18bad071a9c84f2f5d715f992a8e2cbe016fa
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2024-07-02 23:52:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3002;
 
-connect(
-  "mongodb+srv://auth-admin:t2YuYPDKGPIztKib@cluster0.s875rof.mongodb.net/Kriti2024(Dihing)"
-);
 
 app.use(cors({origin:['https://dihingatkriti2024.vercel.app','https://collampus.vercel.app','http://localhost:3000']}));
 app.use(bodyParser.json());
